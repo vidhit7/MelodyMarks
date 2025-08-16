78ec007ac0f8341a855aca89ae00b85f2c569a28
vidhit7 <vidhit.navani31@gmail.com>
2025-08-16 19:15:23 +0530

    chords

---
+++ src/assests/dataSource/editor.js
@@ -0,0 +1,35 @@
+export default {
+  content: `{meta: Original: 1=D}
+{meta: Select: 1=C}
+{meta: Capo=2}
+
+{comment: Prelude/Interlude}
+{start_of_tab}
+|-------|-------|-------|-------|
+|3------|3------|3------|3------|
+|--0---0|--0---0|--0---0|--0---0|
+|----2--|----2--|----2--|----2--|
+|3------|3------|3------|3------|
+|----0--|----0--|----0--|----0--|
+{end_of_tab}
+
+{comment: Main A}
+[C] Pehla Nasha[Em7] Pehla Khumar[F]
+[C] naya pyar hai[Em7] naya intezar[C]
+
+[C]Karlu [Em7] Mai Kya [F]Apna haar
+[C]Ae [Em7] Dile [C]Bekaraar
+
+{comment: Main B}
+[C] Usne Baat Ki[Em7] kuch aise dhang se[F]
+[C] Sapne de gaya[Em7] woh hazaro rang ke
+
+
+{comment: Refrain}
+{start_of_chorus}
+[Am]Pehla nasha
+[F]Pehla khumar
+[Am]la la la la～～ [C]la la la la～～
+[Am]la la la la～～ [C]la la la la～～
+{end_of_chorus}`
+}
